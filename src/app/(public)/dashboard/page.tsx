14ab482fdbbd4e5fbadc500be17db99acfd8ac61
ethrealelement <vitalik.safonov@list.ru>
2025-10-11 10:19:48 +0300

    fix

--- src/app/(public)/dashboard/page.tsx
+++ src/app/(public)/dashboard/page.tsx
@@ -12,7 +12,7 @@ export default function DashboardPage() {
   return (
     <div className="p-6">
       <div className="flex items-center gap-2">
-        <h1 className="text-2xl font-bold ">{board?.board.title}</h1>
+        <h1 className="text-2xl font-bold">{board?.board.title}</h1>
         <div className="h-6 w-6 inline-flex items-center justify-center rounded-md hover:bg-accent cursor-pointer transition-colors">
           <Pencil className="h-3.5 w-3.5" />
         </div>
